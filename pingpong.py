5cefa5624319b6feb7f0f150859faf34c9172328
firedragonPrime <158331581+firedragonPrime@users.noreply.github.com>
2024-05-07 18:01:06 +0300

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -1,6 +1,7 @@
 from pygame import *
 from random import randint
 from time import time as timer
+font.init()
 
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed, player_width, player_higth):
@@ -26,29 +27,48 @@ class Player(GameSprite):
         if keys[K_s] and self.rect.y < 655:
             self.rect.y += self.speed
 class Ball(GameSprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, player_width, player_higth, speed_y):
+        super().__init__(player_image, player_x, player_y, player_speed, player_width, player_higth)
+        self.speed_y = speed_y
     def update(self):
-        self.rect.y += self.speed
+        self.rect.y += self.speed_y
         self.rect.x += self.speed
-player1 = Player("platforma.png", 100, 200, 6, 200, 420)
-player2 = Player("platforma.png", 1620, 200, 6, 200, 420)
-ball = Ball("ball.png", 960, 540, 5, 100, 100)
-window = display.set_mode((1920, 1080))
+        if self.rect.y > 980:
+            self.speed_y *= -1
+        if self.rect.y < 0:
+            self.speed_y *= -1
+        if sprite.collide_rect(player1, self) or sprite.collide_rect(player2, self):
+            self.speed *= -1
+player1 = Player("platforma.png", 100, 200, 6, 50, 420)
+player2 = Player("platforma.png", 1620, 200, 6, 50, 420)
+ball = Ball("ball.png", 960, 540, 5, 100, 100, 5)
+window = display.set_mode((0, 0), FULLSCREEN)
 display.set_caption("пинг-понг")
 FPS = 60
 clock = time.Clock()
 background = transform.scale(image.load("ctol-pingpong.png"), (1920, 1080))
 run = True
+game = False
+font1 = font.Font(None, 80)
+lose1 = font1.render("PLAYER 1 LOSE!", True, (180, 0, 0))
+lose2 = font1.render("PLAYER 2 LOSE!", True, (180, 0, 0))
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
-    
-    window.blit(background, (0, 0))
-    player1.reset()
-    player2.reset()
-    ball.reset()
-    player1.update1()
-    player2.update2()
-    ball.update()
+    if game != True:
+        window.blit(background, (0, 0))
+        player1.reset()
+        player2.reset()
+        ball.reset()
+        player1.update1()
+        player2.update2()
+        ball.update()
+        if ball.rect.x < 0:
+            window.blit(lose1, (820, 520))
+            game = True
+        if ball.rect.x > 1820:
+            window.blit(lose2, (820, 520))
+            game = True
     display.update()
     clock.tick(FPS)
